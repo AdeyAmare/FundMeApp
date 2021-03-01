bbb16afa3f864d586e89d44a216e059a6452c8c2
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2021-03-01 07:44:18 +0300

    commit config

---
+++ Frontend/config/config.dart
@@ -0,0 +1,27 @@
+class Config {
+  
+  static String url = 'http://192.168.1.11:9090/v1';
+  static String donorsURL = 'donors';
+  static String donorURL = 'donor';
+  static String donorUpdateURL = 'donor/update';
+  static String donorDeleteURL = 'donor/delete';
+  static String donorLoginURL = 'donor/login';
+  static String recipientsURL = 'recipients';
+  static String recipientURL = 'recipient';
+  static String recipientUpdateURL = 'recipient/update';
+  static String recipientDeleteURL = 'recipient/delete';
+  static String recipientLoginURL = 'recipient/login';
+   static String recipientinfosURL = 'recipientsinfo';
+  static String recipientinfoURL = 'recipientinfo';
+  static String recipientinfoUpdateURL = 'recipientinfo/update';
+  static String recipientinfoDeleteURL = 'recipientinfo/delete';
+   static String categoriesURL = 'categories';
+  static String categoryURL = 'category';
+  // static String recipientinfoUpdateURL = 'recipientinfo/update';
+   static String categoryDeleteURL = 'category/delete';
+
+
+  static int recID;
+  static bool loggedIn = false;
+ 
+}
